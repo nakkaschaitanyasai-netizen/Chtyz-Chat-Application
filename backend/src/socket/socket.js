7e5e9d6f80b2df335248c8948d603049b2b82aee
nakkaschaitanyasai-netizen <nakkaschaitanyasai@gmail.com>
2026-06-03 23:39:05 +0530

    Fix socket cors

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -7,7 +7,7 @@ let io;
 export const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["https://chtyz-chat-application.vercel.app","http://localhost:5173"],
       methods: ["GET", "POST"],
     },
   });
