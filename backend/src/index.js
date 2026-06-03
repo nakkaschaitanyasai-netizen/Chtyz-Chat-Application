ca5a5a01e197324d6840a8cb8b9c3c47365a314e
nakkaschaitanyasai-netizen <nakkaschaitanyasai@gmail.com>
2026-06-03 23:16:12 +0530

    Add start script

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api", messageRoutes);
