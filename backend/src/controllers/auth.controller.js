40ab5958565865dd132c7d5c660f7f5e12a5c1b3
nakkaschaitanyasai-netizen <nakkaschaitanyasai@gmail.com>
2026-06-04 00:18:49 +0530

    Fix console.error.message

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
     res.status(200).json({ok: true, message: "Login successful", token});
 
   } catch (error) {
-    res.status(500).json({ ok: false, error: "Login failed" });
+    res.status(500).json({ ok: false, error: error.message || "Login failed" });
   }
 };
 
